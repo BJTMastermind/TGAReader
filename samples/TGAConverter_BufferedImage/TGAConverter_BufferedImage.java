9338a77fa63eb20956d883b47cd5420d2b1b7bf3
BJTMastermind <bjtmastermind@gmail.com>
2022-11-29 17:44:45 -0500

    Cleaned up code of samples.

--- samples/TGAConverter_BufferedImage/TGAConverter_BufferedImage.java
+++ samples/TGAConverter_BufferedImage/TGAConverter_BufferedImage.java
@@ -10,26 +10,22 @@ import net.npe.tga.TGAWriter;
 
 
 public class TGAConverter_BufferedImage {
-	
-	public static void main(String [] args) {
-		
-		String path = "images/Mandrill.bmp";
-		
-		try {
-			BufferedImage image = ImageIO.read(new File(path));
-			int width = image.getWidth();
-			int height = image.getHeight();
-			int [] pixels = image.getRGB(0, 0, width, height, null, 0, width);
-			
-			byte [] buffer = TGAWriter.write(pixels, width, height, TGAReader.ARGB);
-			FileOutputStream fos = new FileOutputStream(path.replace(".bmp", ".tga"));
-			fos.write(buffer);
-			fos.close();
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-	}
 
+    public static void main(String[] args) {
+        String path = "images/Mandrill.bmp";
+
+        try {
+            BufferedImage image = ImageIO.read(new File(path));
+            int width = image.getWidth();
+            int height = image.getHeight();
+            int[] pixels = image.getRGB(0, 0, width, height, null, 0, width);
+
+            byte[] buffer = TGAWriter.write(pixels, width, height, TGAReader.ARGB);
+            FileOutputStream fos = new FileOutputStream(path.replace(".bmp", ".tga"));
+            fos.write(buffer);
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- samples/TGASwingBufferedImage/TGASwingBufferedImage.java
+++ samples/TGASwingBufferedImage/TGASwingBufferedImage.java
@@ -1,73 +1,73 @@
 /**
  * TGASwingBufferedImage.java
- * 
+ *
  * Copyright (c) 2014 Kenji Sasaki
  * Released under the MIT license.
  * https://github.com/npedotnet/TGAReader/blob/master/LICENSE
- * 
+ *
  * English document
  * https://github.com/npedotnet/TGAReader/blob/master/README.md
- * 
+ *
  * Japanese document
  * http://3dtech.jp/wiki/index.php?TGAReader
- * 
+ *
  */
 
 import java.awt.Color;
 import java.awt.image.BufferedImage;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import net.npe.tga.TGAReader;
+
 public class TGASwingBufferedImage {
-	
-	private static final String [] TGA_PATHS = {
-		"images/rgb_LL.tga",
-		"images/grayscale_LL.tga",
-		"images/indexed_LL.tga",
-		"images/rgb_a_rle_LL.tga",
-		"images/grayscale_a_rle_LL.tga",
-		"images/indexed_a_rle_LL.tga",
-	};
-	
-	public static void main(String [] args) {
-		try {
-			JPanel panel = new JPanel();
-			panel.setBackground(Color.ORANGE);
-			for(int i=0; i<TGA_PATHS.length; i++) {
-				panel.add(createTGALabel(TGA_PATHS[i]));
-			}
-			JFrame frame = new JFrame();
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.setTitle("TGA Swing BufferedImage");
-			frame.setSize(420, 310);
-			frame.setVisible(true);
-			frame.getContentPane().add(panel);
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private static JLabel createTGALabel(String path) throws IOException {
-		
-		FileInputStream fis = new FileInputStream(path);
-		byte [] buffer = new byte[fis.available()];
-		fis.read(buffer);
-		fis.close();
-		
-		int [] pixels = TGAReader.read(buffer, TGAReader.ARGB);
-		int width = TGAReader.getWidth(buffer);
-		int height = TGAReader.getHeight(buffer);
-		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-		image.setRGB(0, 0, width, height, pixels, 0, width);
-		
-		ImageIcon icon = new ImageIcon(image.getScaledInstance(128, 128, BufferedImage.SCALE_SMOOTH));
-		return new JLabel(icon);
-	}
+
+    private static final String[] TGA_PATHS = {
+        "images/rgb_LL.tga",
+        "images/grayscale_LL.tga",
+        "images/indexed_LL.tga",
+        "images/rgb_a_rle_LL.tga",
+        "images/grayscale_a_rle_LL.tga",
+        "images/indexed_a_rle_LL.tga",
+    };
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+
+        try {
+            for(int i = 0; i < TGA_PATHS.length; i++) {
+                panel.add(createTGALabel(TGA_PATHS[i]));
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        panel.setBackground(Color.ORANGE);
+        frame.setTitle("TGA Swing BufferedImage");
+        frame.setSize(420, 310);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(panel);
+        frame.setVisible(true);
+    }
+
+    private static JLabel createTGALabel(String path) throws IOException {
+        byte[] buffer = Files.readAllBytes(Paths.get(path));
+
+        int[] pixels = TGAReader.read(buffer, TGAReader.ARGB);
+        int width = TGAReader.getWidth(buffer);
+        int height = TGAReader.getHeight(buffer);
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        image.setRGB(0, 0, width, height, pixels, 0, width);
+
+        ImageIcon icon = new ImageIcon(image.getScaledInstance(128, 128, BufferedImage.SCALE_SMOOTH));
+        return new JLabel(icon);
+    }
 
 }
